fe608a8618da0f6eb2eb98b0d4863d6b129e02dd
Luma Lynai <92832671+LumaLynai@users.noreply.github.com>
2023-05-29 00:09:58 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -178,13 +178,13 @@ dic = (
   'WIZS3': 'WIZS3', 'WLM': 'WLMM4', 'YDUQS': 'YDUQ3', 'Zamp': 'ZAMP3'})
 
 t = dic[opcao]
-st.sidebar.components.html("""
-<div vw class="enabled">
+components.html("""
+
     <div vw-access-button class="active"></div>
     <div vw-plugin-wrapper>
       <div class="vw-plugin-top-wrapper"></div>
     </div>
-  </div>
+
   <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
   <script>
     new window.VLibras.Widget('https://vlibras.gov.br/app');
